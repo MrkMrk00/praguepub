5ff9eeca19ed8cfef491111b278b6b5c5fcbd30d
MrkMrk00 <marekhauschwitz@windowslive.com>
2021-11-22 16:50:50 +0100

    added password encryption

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module praguePub.main {
     requires javafx.controls;
     requires static lombok;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.bson;
     exports cz.vse.praguePub.start;
 }
