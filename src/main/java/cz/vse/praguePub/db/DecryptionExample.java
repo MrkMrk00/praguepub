20dc9024871910833afac63009a8b8efc1a52828
MrkMrk00 <marekhauschwitz@windowslive.com>
2021-12-01 10:39:00 +0100

    modified decryption

--- src/main/java/cz/vse/praguePub/db/DecryptionExample.java
+++ src/main/java/cz/vse/praguePub/db/DecryptionExample.java
@@ -1,10 +1,12 @@
 package cz.vse.praguePub.db;
 
+
 import static cz.vse.praguePub.db.AesUtil.fillTo16Chars;
 
 public class DecryptionExample {
 
     public static void main(String[] args) {
+
         var au = new AesUtil(128, 1000);
         var res = au.decrypt(
                 "fbcfcdb5bae8eedf3fa4702bcd1d7d89::5bb737fe5c00850297cd20ef22a72920::yh8TxrKmBUN8mCoPkyNPeg==",
