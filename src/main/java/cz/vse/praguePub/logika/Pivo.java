a2ed1716f7e322dcc7f4901571ebcf5b1a1336c8
staj20 <staj20@vse.cz>
2021-12-10 10:10:05 +0100

    pridano pivo

--- src/main/java/cz/vse/praguePub/logika/Pivo.java
+++ src/main/java/cz/vse/praguePub/logika/Pivo.java
@@ -1,7 +1,13 @@
 package cz.vse.praguePub.logika;
 
 public class Pivo {
+    String nazevPivovaru;
+    double stupnovitost;
+    double obsahAlkholu;
+    String typ;
+    String typKvaseni;
+    String nazev;
     public String getNazev() {
-        return null;
+        return nazev;
     }
 }
