7d8abc5c431b27b6a9ae5e04965a926329dcee68
staj20 <staj20@vse.cz>
2021-12-10 09:50:20 +0100

    zalozen podnik

--- src/main/java/cz/vse/praguePub/logika/Podnik.java
+++ src/main/java/cz/vse/praguePub/logika/Podnik.java
@@ -1,7 +1,37 @@
 package cz.vse.praguePub.logika;
 
-public class Podnik {
-    String nazev;
-    //enum lokace
+import java.util.List;
+import java.util.Map;
 
+public class Podnik {
+    final String nazev;
+    //enum lokace
+    final String adresa;
+    final List<Recenze> recenze;
+    final Map<String,Object> pivniListek;
+
+    public Podnik(String nazev, String adresa, List<Recenze> recenze, Map<String, Object> pivniListek) {
+        this.nazev = nazev;
+        this.adresa = adresa;
+        this.recenze = recenze;
+        this.pivniListek = pivniListek;
+    }
+
+
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public List<Recenze> getRecenze() {
+        return recenze;
+    }
+
+    public Map<String, Object> getPivniListek() {
+        return pivniListek;
+    }
 }
