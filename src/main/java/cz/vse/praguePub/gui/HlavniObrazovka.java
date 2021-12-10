ec88e33d9644d2dee0ef14a349ab4a12370fc3c9
burv05 <burv05vse.cz>
2021-12-10 10:16:55 +0100

    Základ hlavniObrazovky

--- src/main/java/cz/vse/praguePub/gui/HlavniObrazovka.java
+++ src/main/java/cz/vse/praguePub/gui/HlavniObrazovka.java
@@ -1,4 +1,19 @@
 package cz.vse.praguePub.gui;
 
-public class HlavniObrazovka {
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class HlavniObrazovka    {
+    public HlavniObrazovka(BorderPane borderPane) {
+        this.borderPane = borderPane;
+       Stage HlObr = new Stage();
+       HlObr.setScene(new Scene(borderPane));
+       HlObr.show();
+    }
+
+    BorderPane borderPane = new BorderPane();
+
+
+
 }
