9c9839fdd8d86c3801e6533ef68f1b79360485c2
mhau <haum04@vse.cz>
2022-01-09 21:35:02 +0100

    pridan kod pro testovaci sposteni obrazovek

--- src/main/java/cz/vse/praguePub/start/FXApp.java
+++ src/main/java/cz/vse/praguePub/start/FXApp.java
@@ -1,6 +1,10 @@
 package cz.vse.praguePub.start;
 
 import cz.vse.praguePub.gui.ObrazovkyController;
+import cz.vse.praguePub.gui.obrazovky.ZobrazitPodnik;
+import cz.vse.praguePub.logika.Databaze;
+import cz.vse.praguePub.logika.Uzivatel;
+import cz.vse.praguePub.util.PraguePubDatabaseException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,6 +16,21 @@ public class FXApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Databaze db = null;
+        try {
+             db = Databaze.get(Uzivatel.guest());
+        } catch (PraguePubDatabaseException e) {
+            e.printStackTrace();
+        }
+        if (db == null) {
+            System.out.println("dosralo se to");
+            return;
+        }
+
+        var st = new Stage();
+        st.setScene(new ZobrazitPodnik().getScene());
+        st.showAndWait();
+
         new ObrazovkyController().zapniAplikaci(primaryStage);
     }
 }
